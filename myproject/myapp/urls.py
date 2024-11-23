defaee1b9dbe0d8d9008535f44359589b1362857
sampathNG <jnanisampath@gmail.com>
2024-11-23 17:47:34 +0530

    done

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('hello/', views.HelloWorldView.as_view()),
+    path('/', views.HelloWorldView.as_view()),
     path('users/', views.CreateUserView.as_view()),
     path('users/<str:username>/', views.GetUserView.as_view()),
     path('user/all/', views.GetAllUsersView.as_view()),
